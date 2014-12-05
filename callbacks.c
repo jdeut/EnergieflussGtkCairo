96d991530100002fe27cd6d7b3b763dec4af6e77
energienetzwerk Admin <johannes.deutsch@uni-due.de>
2014-12-05 17:11:22 +0100

    adjust drawing of connection to scrolled state of scrolled window

--- callbacks.c
+++ callbacks.c
@@ -1,22 +1,26 @@
 #include "app.h"
 #include "callbacks.h"
 
-gboolean layout_draw_cb(GtkWidget *widget, cairo_t *cr, App *app) {
+gboolean
+layout_draw_cb (GtkWidget * widget, cairo_t * cr, App * app)
+{
     GtkAllocation alloc_source, alloc_end;
+    GtkAdjustment *hadjustment, *vadjustment;
 
     gfloat x_source, y_source, height_source, width_source;
     gfloat x_end, y_end, height_end, width_end;
-    double x0, y0, x1, y1, x2, y2, x3, y3, y_tip, x_tip;
+    double x0, y0, x1, y1, y_tip, x_tip;
     double arrow_tip_width, arrow_tip_length, alpha;
 
     PangoLayout *layout;
     PangoFontDescription *desc;
 
-    GET_UI_ELEMENT(GtkWidget, eventbox1);
-    GET_UI_ELEMENT(GtkWidget, eventbox2);
+    GET_UI_ELEMENT (GtkWidget, eventbox1);
+    GET_UI_ELEMENT (GtkWidget, eventbox2);
+    GET_UI_ELEMENT (GtkWidget, scrolledwindow2);
 
-    gtk_widget_get_allocation(eventbox1, &alloc_source);
-    gtk_widget_get_allocation(eventbox2, &alloc_end);
+    gtk_widget_get_allocation (eventbox1, &alloc_source);
+    gtk_widget_get_allocation (eventbox2, &alloc_end);
 
     x_source = alloc_source.x;
     y_source = alloc_source.y;
@@ -31,42 +35,59 @@ gboolean layout_draw_cb(GtkWidget *widget, cairo_t *cr, App *app) {
     x0 = x_source + width_source;
     y0 = y_source + height_source / 2;
 
-    x3 = x_end;
-    y3 = y_end + height_end / 2;
+    x1 = x_end;
+    y1 = y_end + height_end / 2;
 
     arrow_tip_width = 10;
     arrow_tip_length = 50;
-    alpha = atan2f (y3 - y0, x3 - x0);
+    alpha = atan2f (y1 - y0, x1 - x0);
 
+    hadjustment = gtk_scrolled_window_get_hadjustment(GTK_SCROLLED_WINDOW(scrolledwindow2));
+    vadjustment = gtk_scrolled_window_get_vadjustment(GTK_SCROLLED_WINDOW(scrolledwindow2));
+
+    gfloat hval =  gtk_adjustment_get_value(hadjustment);
+    gfloat vval =  gtk_adjustment_get_value(vadjustment);
+
+    /* *
+     * Arrow tip
+     * */
+    cairo_translate (cr, -hval, -vval);
     cairo_set_source_rgb (cr, 0, 1, 0);
-    cairo_translate (cr, x3, y3);
-    cairo_rotate (cr, alpha);
-    cairo_move_to (cr, 0, 0);
-    cairo_line_to (cr, -arrow_tip_length, -arrow_tip_width);
-    cairo_line_to (cr, -arrow_tip_length, arrow_tip_width);
-    cairo_fill (cr);
 
-    x3 = -arrow_tip_length / 2;
-    y3 = 0;
+    cairo_save(cr);
 
-    cairo_user_to_device (cr, &x3, &y3);
+        cairo_translate (cr, x1, y1);
+        cairo_rotate (cr, alpha);
+        cairo_move_to (cr, 0, 0);
+        cairo_line_to (cr, -arrow_tip_length, -arrow_tip_width);
+        cairo_line_to (cr, -arrow_tip_length, arrow_tip_width);
 
-    x_tip = x3;
-    y_tip = (y3 - y0) / (x3 - x0) * (x_tip - x0) + y0;
+        cairo_fill (cr);
 
-    cairo_identity_matrix (cr);
+    /* *
+     * Connection line
+     * */
 
-    cairo_move_to (cr, x0, y0);
-    cairo_curve_to (cr, x0 + 40, y0, x_tip, y_tip, x_tip,
-                    y_tip);
+        cairo_move_to (cr, -arrow_tip_length / 2, 0);
+
+    cairo_restore(cr);
+
+
+    cairo_line_to (cr, x0, y0);
     cairo_set_line_width (cr, 5);
     cairo_stroke (cr);
+
+    /* *
+     * Beschriftung
+     * */
 
     cairo_set_source_rgb (cr, 0, 0.5, 0);
 
     layout = pango_cairo_create_layout (cr);
 
-    pango_layout_set_markup (layout, "<span gravity=\"south\" weight=\"550\" size=\"x-large\">W<sub>el</sub></span>", -1);
+    pango_layout_set_markup (layout,
+                             "<span gravity=\"south\" weight=\"550\" size=\"x-large\">W<sub>el</sub></span>",
+                             -1);
     pango_cairo_update_layout (cr, layout);
 
     cairo_move_to (cr, x0, y0);
@@ -74,4 +95,3 @@ gboolean layout_draw_cb(GtkWidget *widget, cairo_t *cr, App *app) {
 
     return FALSE;
 }
-
